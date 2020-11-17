f66616253ce0c375255ad50e228dfcde2b7744c0
Eli Wang <sirxenofex@gmail.com>
2020-11-17 08:51:48 +0800

    增加Select元素

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./styles.css";
-import { Form, TextInput, Validations } from "./form";
+import { Form, TextInput, Select, Validations } from "./form";
 
 export default function App() {
   const defaultUser = {
@@ -9,6 +9,12 @@ export default function App() {
   const { present, email } = Validations;
   const [user, setUser] = useState(defaultUser);
 
+  const sexOptions = [
+    { name: "男", value: "male" },
+    { name: "女", value: "female" },
+    { name: "未知", value: "unknown" }
+  ];
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
@@ -16,6 +22,7 @@ export default function App() {
       <Form bind={user} onChange={(user) => setUser(user)}>
         <TextInput name="name" validations={[present]} />
         <TextInput name="email" validations={[present, email]} />
+        <Select name="sex" options={sexOptions} />
         <button onClick={() => console.log("user: ", user)}>SUBMIT</button>
       </Form>
     </div>
--- src/form.js
+++ src/form.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useContext } from "react";
+import PropTypes from "prop-types";
 
 const BoundContext = createContext(null);
 
@@ -56,6 +57,67 @@ function TextInput({ value: defaultValue, title, name, validations }) {
   );
 }
 
+function Select({
+  value: defaultValue,
+  title,
+  name,
+  validations = [],
+  options
+}) {
+  const { boundObject, onBoundObjectChange } = useContext(BoundContext);
+  const [error, setError] = useState(null);
+  const [value, setValue] = useState(defaultValue || boundObject[name]);
+  const [changed, setChanged] = useState(false);
+
+  const onChange = ({ currentTarget }) => {
+    const { value } = currentTarget;
+    setValue(value);
+    setChanged(true);
+    boundObject[name] = value;
+    onBoundObjectChange(boundObject);
+
+    const errors = [];
+
+    validations.forEach((validation) => {
+      const e = validation(value);
+      if (e) {
+        errors.push(e);
+      }
+    });
+
+    if (errors.length > 0) {
+      setError(errors.join(", "));
+    } else {
+      setError(null);
+    }
+  };
+
+  return (
+    <div className="form-control">
+      <label className="form-label" htmlFor={"label-" + title}>
+        {title || name}
+      </label>
+      <select onChange={onChange}>
+        {options.map(({ name, value: val }) => (
+          <option selected={value === val && "selected"} val={val}>
+            {name}
+          </option>
+        ))}
+      </select>
+      {changed && error && <div className="form-error">{error}</div>}
+    </div>
+  );
+}
+
+Select.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.exact({
+      name: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired
+    })
+  )
+};
+
 const Validations = {
   present(value) {
     if (!value) {
@@ -74,4 +136,4 @@ const Validations = {
   }
 };
 
-export { Form, TextInput, Validations };
+export { Form, TextInput, Select, Validations };
